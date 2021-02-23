51124ea6370b4c74deb8526c31a7f4801307172a
akak5211 <akak5211@dgu.ac.kr>
2021-02-23 16:51:09 +0900

    ejs  엔진 사용

--- expressServer.js
+++ expressServer.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const app = express()
- 
+
+//뷰 파일이 있는 디렉토리 설정
+app.set('views', __dirname + '/views');
+//뷰 엔진으로 ejs를 사용한다
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World')
 })
 
+app.get('/ejs', function (req, res) {
+    res.render('ejsTest')
+})
+  
 app.get('/user', function (req, res) {
-    res.send('사용자 페이지 입니다')
-  })
+      connection.query('SELECT * FROM user;', function (error, results, fields) {
+          console.log(results);
+          res.send(results)
+    })
+});
+  
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'dgu1234!',
+  database : '210222finteche'
+});
+ 
+connection.connect();
+
+//connection.end();
 
 app.listen(3000)
