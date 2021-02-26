adb0f5f8a0efddd26a6c61a7ea2a2fee1df5060a
akak5211 <akak5211@dgu.ac.kr>
2021-02-26 14:34:21 +0900

    입금이체

--- expressServer.js
+++ expressServer.js
@@ -295,7 +295,10 @@ app.post('/withdraw', auth, function(req, res){
     var sql = "SELECT * FROM user WHERE id = ?";
     var countnum = Math.floor(Math.random() * 1000000000) + 1;
     var transId = companyId + countnum;  
-    var transdtime = moment(new Date()).format('YYYYMMDDhhmmss');
+    var transdtime = moment(new Date()).format('YYYYMMDDhhmmss')
+    var amount = req.body.amount;
+    var fin_use_num = req.body.fin_use_num;
+    var to_fin_use_num = req.body.to_fin_use_num;
     console.log(transdtime);
     connection.query(sql,[user.userId], function(err, result){
         if(err) throw err;
@@ -334,13 +337,52 @@ app.post('/withdraw', auth, function(req, res){
                 else {
                     var transactionListResuult = body;
                     console.log(transactionListResuult);
-                    res.json(transactionListResuult)
+                    if(transactionListResuult.rsp_code === "A0000"){
+                        var countnum2 = Math.floor(Math.random() * 1000000000) + 1;
+                        var transId2 = companyId + countnum2;  
+                        var transdtime2 = moment(new Date()).format('YYYYMMDDhhmmss');                    
+                        var option = {
+                            method : "POST",
+                            url : "https://testapi.openbanking.or.kr/v2.0/transfer/deposit/fin_num",
+                            headers : {
+                              Authorization : "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJNMjAyMTExNTk1Iiwic2NvcGUiOlsib29iIl0sImlzcyI6Imh0dHBzOi8vd3d3Lm9wZW5iYW5raW5nLm9yLmtyIiwiZXhwIjoxNjIyMDg1NjUzLCJqdGkiOiIwODNmNWNlMS0zYzgyLTQ1NWItOTdhNS0yZjQ2NDUwMmQ4Y2MifQ.zScQsGyGS7T4sxOH9oflOCsKCkAwlfLIv0__QWFaAQk"
+                            },
+                            //get 요청을 보낼때 데이터는 qs, post 에 form, json 입력가능
+                            json : {
+                              "cntr_account_type": "N",
+                              "cntr_account_num": "123123123123",
+                              "wd_pass_phrase": "NONE",
+                              "wd_print_content": "환불금액",
+                              "name_check_option": "off",
+                              "tran_dtime": transdtime2,
+                              "req_cnt": "1",
+                              "req_list": [
+                                {
+                                  "tran_no": "1",
+                                  "bank_tran_id": transId2,
+                                  "fintech_use_num": req.body.to_fin_use_num,
+                                  "print_content": "쇼핑몰환불",
+                                  "tran_amt": req.body.amount,
+                                  "req_client_name": "홍길동",
+                                  "req_client_num": "HONGGILDONG1234",
+                                  "req_client_fintech_use_num": req.body.fin_use_num,
+                                  "transfer_purpose": "ST"
+                                }
+                              ]
+                            }
+                        }
+                        request(option, function (error, response, body) {
+                            console.log(body);
+                            res.json(body);
+                        });
+                
+                    }
+                    //입금 api 실행 A0000 res_code 입급이체 발생
                 }
             })        
         }
     })
 })
-
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
